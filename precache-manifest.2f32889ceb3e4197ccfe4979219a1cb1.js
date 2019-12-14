e7a189064a05815828c554a962f6d78b87187ba5
葉宗翰 <02090560@me.mcu.edu.tw>
2019-12-15 00:14:08 +0800

    Updates

---
+++ precache-manifest.2f32889ceb3e4197ccfe4979219a1cb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520e5a835f96cd56a45643a7e7d70ec8",
+    "url": "/uniarsal/index.html"
+  },
+  {
+    "revision": "2a042ef9f622260615bc",
+    "url": "/uniarsal/static/css/main.1da8a160.chunk.css"
+  },
+  {
+    "revision": "8ef8aa0dccd0f0474b3b",
+    "url": "/uniarsal/static/js/2.cacb2c3a.chunk.js"
+  },
+  {
+    "revision": "2a042ef9f622260615bc",
+    "url": "/uniarsal/static/js/main.82a0bf03.chunk.js"
+  },
+  {
+    "revision": "7c6b7bec7cbfd2be9170",
+    "url": "/uniarsal/static/js/runtime-main.91c082c7.js"
+  }
+]);
